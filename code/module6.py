9601ffea25559a5719a91507d0c21aa8ded7ba13
danielnalley <danielnalley11@gmail.com>
2017-05-03 13:21:36 -0400

    Create module6.py

---
+++ code/module6.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+dict = {'Name': 'Daniel', 'Age': 16, 'Height': 2ft}
+
+print "dict['Name']: ", dict['Name']
+print "dict['Age']: ", dict['Age']
+print "dict['Height']: ", dict['Height']
