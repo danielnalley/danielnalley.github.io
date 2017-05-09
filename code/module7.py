3cc5a5ace336b80af3cd210cac6f1ed8e4dc006d
danielnalley <danielnalley11@gmail.com>
2017-05-09 13:28:23 -0400

    Create module7.py

---
+++ code/module7.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+dict = {'Level': '27', 'EXP': ‘41,349’, ‘EXP Needed’: ‘43,500’, 'Name': ‘DankBoi’, ‘Skill’: ‘Dual Wield’}
+
+print "dict[‘Level’]: ", dict[‘Level’]
+print "dict[‘EXP’]: ", dict[‘EXP’]
+print "dict[‘EXP Needed’]: ", dict['EXP Needed']
+print "dict['Name']: ", dict['Name']
+print "dict['Skill']: ", dict['Skill']
